1d70a48192657a6141bd248d93e6d6f789678f88
Sarthak <sarthakdubey1009@gmail.com>
2021-01-17 20:53:27 +0530

    commit

---
+++ Assets/Scripts/Main Scene/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip PuckCollision;
+    public AudioClip Goal;
+    public AudioClip WonGame;
+    public AudioClip LostGame;
+
+    private AudioSource audiosource;
+    private void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
+    public void PlayPuckCollision()
+    {
+        audiosource.PlayOneShot(PuckCollision);
+    }
+    public void PlayGoal()
+    {
+        audiosource.PlayOneShot(Goal);
+    }
+    public void PlayWonGame()
+    {
+        audiosource.PlayOneShot(WonGame);
+    }
+    public void PlayLostGame()
+    {
+        audiosource.PlayOneShot(LostGame);
+    }
+}
---
+++ Assets/Scripts/Main Scene/ScoreScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public enum Score
+    {
+        AiScore,PlayerScore
+    }
+    public int MaxScore;
+    public UiManager uiManager;
+    #region Scores
+    private int aiScore, playerScore;
+    private int AiScore
+    {
+        get { return aiScore; }
+        set
+        {
+            aiScore = value;
+            if(value == MaxScore)
+            {
+                uiManager.ShowRestartCanvas(true);
+            }
+        }
+    }
+    private int PlayerScore
+    {
+        get { return playerScore; }
+        set
+        {
+            playerScore = value;
+            if (value == MaxScore)
+            {
+                uiManager.ShowRestartCanvas(false);
+            }
+        }
+    }
+    #endregion
+    public Text AiScoretxt, PlayerScoretxt;
+    private int aiscore, playerscore;
+    public void Increment(Score whichScore)
+    {
+        if (whichScore == Score.AiScore)
+            AiScoretxt.text = (++AiScore).ToString();
+        else
+            PlayerScoretxt.text = (++PlayerScore).ToString();
+    }
+    public void ResetScores()
+    {
+        AiScore = PlayerScore = 0;
+        AiScoretxt.text = PlayerScoretxt.text = "0";
+    }
+}
